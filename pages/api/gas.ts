80162c1d2b8f9748632fcc2dffde39911fcd4ef1
jojo-syl <jojokim@syllabs.io>
2022-12-06 22:32:27 +0900

    feat: get current Eth Gas with sse

---
+++ pages/api/gas.ts
@@ -0,0 +1,31 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ethers } from "ethers";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+const currentGasFee = async () => {
+  const provider = new ethers.providers.EtherscanProvider("mainnet", process.env.ETHERSCAN_KEY || "");
+  try {
+    return (await provider.getGasPrice()).toNumber();
+  } catch (e) {
+    return -1000000000;
+  }
+};
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive",
+  });
+
+  setInterval(async function () {
+    const gas = await currentGasFee();
+    res.write("data: " + gas);
+    console.log(gas);
+    res.flush();
+  }, 1000);
+}
